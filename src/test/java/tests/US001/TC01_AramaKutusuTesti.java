d8560bdd3594bc2aed24a3566a55e0558abdcff5
kemal <mkemalyavuzm@gmail.com>
2023-07-26 07:54:30 +0300

    US001_V1

---
+++ src/test/java/tests/US001/TC01_AramaKutusuTesti.java
@@ -0,0 +1,17 @@
+package tests.US001;
+
+import org.junit.Test;
+import org.openqa.selenium.Keys;
+import pages.Page;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+public class TC01_AramaKutusuTesti {
+    @Test
+    public void test01() {
+        Driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        Page page = new Page();
+        page.aramaKutusu.sendKeys("iphone", Keys.ENTER);
+
+    }
+}
